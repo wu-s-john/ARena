7fbf26238e3fc03ac456553261bf194b497b7732
Kirk <kirkperkins99@gmail.com>
2018-03-14 15:05:32 -0700

    Made clickable action to drop board soon

---
+++ ARena-development/Assets/Debug.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Debug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // OnClick code goes here ...
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
