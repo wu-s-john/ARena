fc343dc0ab8ae01bdd683ae3a5216af0f37bff88
Kirk <kirkperkins99@gmail.com>
2018-03-14 07:58:52 -0700

    saved my life

---
+++ ARena-development/Assets/ButtonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerClickHandler {
+
+    public GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello");
+	}
+    
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space key was pressed");
+        }
+    }
+
+    //Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        //Use this to tell when the user right-clicks on the Button
+        if (pointerEventData.button == PointerEventData.InputButton.Right)
+        {
+            //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+            Debug.Log(name + " Game Object Right Clicked!");
+        }
+
+        //Use this to tell when the user left-clicks on the Button
+        if (pointerEventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log(name + " Game Object Left Clicked!");
+        }
+    }
+
+    public void Message() {
+        Debug.Log("Hello world");
+    }
+}
